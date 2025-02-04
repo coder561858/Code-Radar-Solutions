61e1fcce15bba77a548354ee90295c037a8f323d
AadiMalik <maadi9396@gmail.com>
2025-02-04 11:42:04 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
     int main(){
         int x,y,z;
-        scanf("%d,%d,%d",&x,&y,&z);
+        scanf("%d %d %d",&x,&y,&z);
         if((x*x)+(y*y)=(z*z)){
         printf("Valid");
         }
