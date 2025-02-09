14ca8037c40089cc84a93f944bf3643151ac0f92
AadiMalik <maadi9396@gmail.com>
2025-02-09 16:01:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,11 +2,11 @@
 int main(){
     int x,y,z;
     scanf("%d %d %d",&x,&y,&z);
-    if(z== +){
+    if(z== "+"){
        int sum=x+y;
        printf("%d",sum);
     }
-    else if(z=='-'){
+    else if(z== "-"){
         int sub=x-y;
         printf("%d",sub);
     }
