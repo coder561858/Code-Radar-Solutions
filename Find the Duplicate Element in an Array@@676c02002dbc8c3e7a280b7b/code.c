9e2bba1e7302af45694b459b866d5a515bbafc9f
Aadi Malik <maadi9396@gmail.com>
2025-04-06 15:34:18 +0530

    Solution to Find the Duplicate Element in an Array

--- Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
+++ Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
@@ -1,14 +1,21 @@
 // Your code here...
-#include<stdio.h>
-int main(){
-	int n;
-	scanf("%d",&n);
-	int arr[n];
-	int i;
-	for(i=0;i<n-1;i++){
-		scanf("%d",&arr[i]);
-		if(arr[i]==arr[n-i+1]){
-			printf("%d",arr[i]);
-		}
-	}
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Compare symmetric elements
+    for (int i = 0; i < n / 2; i++) {
+        if (arr[i] == arr[n - i - 1]) {
+            printf("Symmetric element at %d and %d: %d\n", i, n - i - 1, arr[i]);
+        }
+    }
+
+    return 0;
 }
