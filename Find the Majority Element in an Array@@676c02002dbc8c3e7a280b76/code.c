6ab9a9a0d01027a02bd3bba763d6d8ada5b510e2
Aadi Malik <maadi9396@gmail.com>
2025-04-07 14:19:17 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -21,7 +21,7 @@ int main() {
         else if(a[i]<a[n]/2){
             count=-1;
         }
-        else if(a[i]!=a[n-i-1]){
+        else if(a[n-1]==3){
             count=-1;
         }
         
