adb533d77c5ba6a64b0d7d0c026e264301d29bc6
Aadi Malik <maadi9396@gmail.com>
2025-04-08 21:54:18 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -4,7 +4,7 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    if (n < 3) { // You need at least 3 elements to check neighbors
+    if (n < 3) {
         printf("-1\n");
         return 0;
     }
@@ -14,17 +14,21 @@ int main() {
         scanf("%d", &a[i]);
     }
 
-    int found = 0; // initialize flag
+    int found = 0;
 
+    // Check from index 1 to n-2
     for (int i = 1; i < n - 1; i++) {
         if (a[i - 1] < a[i] && a[i] > a[i + 1] && a[0] < a[i]) {
             printf("%d\n", a[i]);
             found = 1;
             break;
-        } else if ( a[i-1]<a[n - 1] && a[n-1]>a[n]) {
-            // Optional: handle special case if needed
-            found = 0; // this doesn't help unless you print something based on it
         }
+    }
+
+    // Also check the last element (a[n-1])
+    if (!found && a[n - 2] < a[n - 1] && a[0] < a[n - 1]) {
+        printf("%d\n", a[n - 1]);
+        found = 1;
     }
 
     if (!found) {
